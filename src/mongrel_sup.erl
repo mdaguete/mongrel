c07369526cbcdbbf683d9869d3ea90bb9a9a0c41
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-02-20 16:49:30 +0000

    Added supervisor.

---
+++ src/mongrel_sup.erl
@@ -0,0 +1,66 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+%% @author CA Meijer
+%% @copyright 2012 CA Meijer
+%% @doc TODO
+
+-module(mongrel_sup).
+
+-behaviour(supervisor).
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([]).
+
+%% --------------------------------------------------------------------
+%% Internal exports
+%% --------------------------------------------------------------------
+-export([init/1]).
+
+%% --------------------------------------------------------------------
+%% Macros
+%% --------------------------------------------------------------------
+-define(SERVER, ?MODULE).
+
+%% --------------------------------------------------------------------
+%% Records
+%% --------------------------------------------------------------------
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+%% --------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok,  {SupFlags,  [ChildSpec]}} |
+%%          ignore                          |
+%%          {error, Reason}
+%% --------------------------------------------------------------------
+init([]) ->
+    Child = {mongrel, {mongrel, start_link, []},
+	      permanent, 2000, worker, [mongrel]},
+    {ok, {{one_for_all,0,1}, [Child]}}.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
