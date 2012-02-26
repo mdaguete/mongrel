f1e8296e2fbe70801d60c736a318b96332965b4f
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-02-26 09:03:03 +0000

    Added comments to example insert code.

--- test_examples/insert_test.erl
+++ test_examples/insert_test.erl
@@ -4,20 +4,32 @@
 
 -include("mongrel_macros.hrl").
 
+% Our "domain objects" are a book, an author and a review
 -record(book, {'_id', title, isbn, author, reviews}).
 -record(author, {'_id', first_name, last_name}).
 -record(review, {star_rating, comment}).
 
 test() ->
+	application:start(mongodb),
+	application:start(mongrel),
+	
+	% For mongrel to work, we need to specify how to map books, authors and reviews.
 	mongrel_mapper:add_mapping(?mapping(book)),
 	mongrel_mapper:add_mapping(?mapping(author)),
 	mongrel_mapper:add_mapping(?mapping(review)),
+	
+	% Create a sample author and a book record with two reviews.
+	% Notice we use a macro for assigning an _id
 	Author = #author{?id(), last_name= <<"Tolstoy">>},
 	Book = #book{?id(), title= <<"War and Peace">>, author=Author},
-	BookWithReviews = Book#book{reviews = [#review{star_rating=1, comment= <<"Turgid old nonsense">>}, #review{star_rating=5}]},
+	BookWithReviews = Book#book{reviews = [#review{star_rating=1, comment= <<"Turgid old nonsense">>}, 
+										   #review{star_rating=5}]},
+	
+	% Connect to MongoDB
 	Host = {localhost, 27017},
 	{ok, Conn} = mongo:connect(Host),
-	InsertBookFun = fun() ->
-							mongrel:insert(BookWithReviews)
-					end,
-	mongo:do(safe, master, Conn, mongrel_test, InsertBookFun). 
+	
+	% Use the mongrel insert function and the MongoDB driver to write the book record to the database
+	mongo:do(safe, master, Conn, mongrel_test, fun() ->
+													   mongrel:insert(BookWithReviews)
+			 end). 
