3954bc2ed11f1ed2bc0f850ed4542034f1b1d5a7
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-02-26 08:45:57 +0000

    Added a review record to the book review example.

--- test_examples/insert_test.erl
+++ test_examples/insert_test.erl
@@ -4,17 +4,20 @@
 
 -include("mongrel_macros.hrl").
 
--record(book, {'_id', title, isbn, author}).
+-record(book, {'_id', title, isbn, author, reviews}).
 -record(author, {'_id', first_name, last_name}).
+-record(review, {star_rating, comment}).
 
 test() ->
 	mongrel_mapper:add_mapping(?mapping(book)),
 	mongrel_mapper:add_mapping(?mapping(author)),
+	mongrel_mapper:add_mapping(?mapping(review)),
 	Author = #author{?id(), last_name= <<"Tolstoy">>},
 	Book = #book{?id(), title= <<"War and Peace">>, author=Author},
+	BookWithReviews = Book#book{reviews = [#review{star_rating=1, comment= <<"Turgid old nonsense">>}, #review{star_rating=5}]},
 	Host = {localhost, 27017},
 	{ok, Conn} = mongo:connect(Host),
 	InsertBookFun = fun() ->
-							mongrel:insert(Book)
+							mongrel:insert(BookWithReviews)
 					end,
 	mongo:do(safe, master, Conn, mongrel_test, InsertBookFun). 
