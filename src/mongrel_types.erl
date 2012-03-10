3977e5afb8135944674e5e03f9c752ffe9e7c12a
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-03-10 07:30:49 +0000

    Added edoc comments for mongrel_types.

--- src/mongrel_types.erl
+++ src/mongrel_types.erl
@@ -10,19 +10,38 @@
 % License for the specific language governing permissions and limitations under
 % the License.
 
+%%% @author CA Meijer
+%%% @copyright 2012 CA Meijer
+%%% @doc Provides convenience function for encapsulating data types in the form required by
+%%       the Erlang MongoDB driver.
+%%% @end
+
 -module(mongrel_types).
 
-%% Exported Functions
+%% API
 -export([binary/1,
 		 uuid/1,
 		 md5/1]).
 
 %% External functions
+
+%% @doc Encapsulates a binary value as 3-tuple expected by MongoDB driver.
+%%
+%% @spec binary(binary()) -> {bin, bin, X}
+%% @end
 binary(X) when is_binary(X) ->
 	{bin, bin, X}.
 
+%% @doc Encapsulates a UUID as 3-tuple expected by MongoDB driver.
+%%
+%% @spec uuid(binary()) -> {bin, uuid, X}
+%% @end
 uuid(X) when is_binary(X) ->
 	{bin, uuid, X}.
 
+%% @doc Encapsulates an MD5 hash as 3-tuple expected by MongoDB driver.
+%%
+%% @spec md5(binary()) -> {bin, md5, X}
+%% @end
 md5(X) when is_binary(X) ->
 	{bin, md5, X}.
