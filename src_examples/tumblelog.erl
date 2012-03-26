ba50230955ba6b416ef2426ded752af7c30d056e
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-03-26 18:19:57 +0000

    Typo fixes.

--- src_examples/tumblelog.erl
+++ src_examples/tumblelog.erl
@@ -1,14 +1,30 @@
-%%% This example code is a rough port of the MongoEngine example at http://mongoengine.org/docs/v0.5/tutorial.html
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+%% This example code is a rough port of the MongoEngine example at 
+%% http://mongoengine.org/docs/v0.5/tutorial.html
 -module(tumblelog).
 -include_lib("mongrel/include/mongrel_macros.hrl").
 -export([add_mappings/0, populate/0]).
 
+%% Users and posts are stored in collections so they have '_id' fields
 -record(user, {'_id', email, first_name, last_name}).
 -record(post, {'_id', title, author, tags, comments, body}).
--record(comment, {content, name}).
 -record(text_post, {content}).
 -record(image_post, {image_path}).
 -record(link_post, {link_url}).
+
+%% Comments are embedded in posts so do not have an '_id' field.
+-record(comment, {content, name}).
 
 add_mappings() ->
 	?add_mapping(user),
@@ -21,7 +37,7 @@ add_mappings() ->
 populate() ->
 	John = #user{?id(), email= <<"jdoe@example.com">>, first_name= <<"John">>, last_name= <<"Doe">>},
 	Post1 = #post{?id(), title= <<"Fun with MongoeEngine">>, author=John, tags=[<<"mongodb">>, <<"mongoengine">>], 
-				  body=#text_post{content= <<"Took a look at mongoengine today, looks prety cool.">>}},
+				  body=#text_post{content= <<"Took a look at mongoengine today, looks pretty cool.">>}},
 	Post2 = #post{?id(), title= <<"MongoEngine Documentation">>, author=John, tags=[<<"mongoengine">>],
 				  body=#link_post{link_url= <<"http://tractiondigital.com/labs/mongoengine/docs">>}},
 	{ok, Conn} = mongo:connect(localhost),
