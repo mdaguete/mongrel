8ba7b49f713635c59b2fdea50fabaedbe416fe94
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-04-21 09:20:23 +0000

    Can set a timeout for a cursor.

--- src/mongrel_cursor.erl
+++ src/mongrel_cursor.erl
@@ -29,7 +29,8 @@
 		 cursor/5,
 		 next/1,
 		 rest/1,
-		 get_mongo_cursor/1]).
+		 get_mongo_cursor/1,
+		 set_timeout/2]).
 
 %% gen_server callbacks
 -export([init/1, 
@@ -40,7 +41,7 @@
 		 code_change/3]).
 
 %% Records
--record(state, {mongo_cursor, read_mode, connection, database, collection, parent_process}).
+-record(state, {mongo_cursor, read_mode, connection, database, collection, parent_process, timeout=infinity, die_with_parent=true}).
 
 %% Types
 -type(cursor() :: pid()).
@@ -79,6 +80,12 @@ get_mongo_cursor(Cursor) ->
 close(Cursor) ->
 	gen_server:call(Cursor, close, infinity).
 
+%% @doc Sets the cursor timeout. The cursor will die after the specified length of inactivity.
+%%      The cursor remains alive even if the parent process dies.
+-spec(set_timeout(cursor(), integer()|infinity) -> ok).
+set_timeout(Cursor, Timeout) ->
+	gen_server:call(Cursor, {set_timeout, Timeout}, infinity).
+	
 %% Server functions
 
 %% @doc Initializes the cursor with a MongoDB cursor and connection parameters.
@@ -98,15 +105,17 @@ handle_call(next, _From, State) ->
 		{Document} ->
 			CallbackFunc = construct_callback_function(State),
 			Reply = mongrel_mapper:unmap(State#state.collection, Document, CallbackFunc),
-			{reply, Reply, State, infinity}
+			{reply, Reply, State, State#state.timeout}
 	end;
 handle_call(rest, _From, State) ->
 	Docs = rest(State, []),
 	{stop, normal, Docs, State};
 handle_call(get_mongo_cursor, _From, State) ->
-	{reply, State#state.mongo_cursor, State, infinity};
+	{reply, State#state.mongo_cursor, State, State#state.timeout};
 handle_call(close, _From, State) ->
-	{stop, normal, ok, State}.
+	{stop, normal, ok, State};
+handle_call({set_timeout, Timeout}, _From, State) ->
+	{reply, ok, State#state{die_with_parent=false, timeout=Timeout}, Timeout}.
 
 
 %% @doc Responds asynchronously to messages. Asynchronous messages are ignored.
@@ -114,12 +123,12 @@ handle_call(close, _From, State) ->
 handle_cast(_Message, State) ->
 	{noreply, State}.
 
-%% @doc Responds to non-OTP messages. The only out-of-band message of interest is a timeout.
-%%      A timeout indicates that there has been no activity invoking the cursor for a 
-%%      specified time. The cursor process is terminated on a timeout. All other messages are
-%%      ignored.
+%% @doc Responds to non-OTP messages. The messages that are handled are a timeout and the
+%%      the termination of the parent process.
 -spec(handle_info(Message::any(), State::record()) -> {stop, normal, State::record()}|{noreply, State::record()}).
-handle_info({'DOWN', _Ref, process, Pid, _Reason}, State) when Pid =:= State#state.parent_process ->
+handle_info({'DOWN', _Ref, process, Pid, _Reason}, State) when Pid =:= State#state.parent_process andalso State#state.die_with_parent ->
+	{stop, normal, State};
+handle_info(timeout, State) ->
 	{stop, normal, State};
 handle_info(_Info, State) ->
 	{noreply, State}.
